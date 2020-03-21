3ca4cca069a1bf50d7d077dd73f67e313a42ff6c
Carlos Méndez <carlos.mdez@outlook.com>
2020-03-21 13:29:54 -0600

    feat: Add header related components

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ import {
 } from 'react-router-dom'
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace'
+import MainNavigation from './shared/components/Navigation/MainNavigation'
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path='/' exact component={Users} />
-        <Route path='/places/new' exact component={NewPlace} />
-        <Redirect to='/' />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path='/' exact component={Users} />
+          <Route path='/places/new' exact component={NewPlace} />
+          <Redirect to='/' />
+        </Switch>
+      </main>
     </Router>
   )
 }
