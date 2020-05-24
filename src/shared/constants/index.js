ec01a67315bfb0966423c565c57a0b73b9348992
Carlos Méndez <carlos.mdez@outlook.com>
2020-05-24 09:46:36 -0500

    feat: Add useHttpClient hook in Auth component

---
+++ src/shared/constants/index.js
@@ -0,0 +1,13 @@
+export const API_URL = 'http://localhost:5000/api'
+
+//User's API endpoints
+export const GET_USERS_URL = `${API_URL}/users`
+export const SIGNUP_URL = `${API_URL}/users/signup`
+export const LOGIN_URL = `${API_URL}/users/login`
+
+//Place's API endpoints
+export const GET_PLACE_URL = `${API_URL}/places`
+export const GET_PLACES_URL = `${API_URL}/places/user`
+export const CREATE_PLACE_URL = `${API_URL}/places`
+export const UPDATE_PLACE_URL = `${API_URL}/places`
+export const DELETE_PLACE_URL = `${API_URL}/places`
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -3,8 +3,12 @@ import React, { useState, useContext } from 'react'
 import Input from '../../shared/components/FormElements/Input'
 import Button from '../../shared/components/FormElements/Button'
 import Card from '../../shared/components/UIElements/Card'
+import ErrorModal from '../../shared/components/UIElements/ErrorModal'
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
 import useForm from '../../shared/hooks/useForm'
+import useHttpClient from '../../shared/hooks/useHttpClient'
 import { AuthContext } from '../../shared/context/authContext'
+import { LOGIN_URL, SIGNUP_URL } from '../../shared/constants'
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_REQUIRE
@@ -26,6 +30,7 @@ const initialInputs = {
 const Auth = () => {
   const auth = useContext(AuthContext)
   const [isLoginMode, setIsLoginMode] = useState(true)
+  const { isLoading, error, sendRequest, clearError } = useHttpClient()
   const [formState, inputHandler, setFormData] = useForm(initialInputs, false)
 
   const switchModeHandler = () => {
@@ -51,60 +56,92 @@ const Auth = () => {
 
   const authSubmitHandler = event => {
     event.preventDefault()
-    console.log(formState.inputs)
-    auth.login()
+    if (isLoginMode) {
+      sendRequest(
+        LOGIN_URL,
+        'POST',
+        { 'Content-Type': 'application/json' },
+        JSON.stringify({
+          email: formState.inputs.email.value,
+          password: formState.inputs.password.value
+        })
+      ).then(res => {
+        console.log(res)
+        auth.login()
+      })
+      .catch(err => console.log(err))
+    } else {
+      sendRequest(
+        SIGNUP_URL,
+        'POST',
+        { 'Content-Type': 'application/json' },
+        JSON.stringify({
+          name: formState.inputs.name.value,
+          email: formState.inputs.email.value,
+          password: formState.inputs.password.value
+        })
+      ).then(res => {
+        console.log(res)
+        auth.login()
+      })
+      .catch(err => console.log(err))
+    }
   }
 
   return (
-    <Card className='authentication'>
-      <h2>{isLoginMode ? 'Log In' : 'Sign Up'}</h2>
-      <hr />
-      <form onSubmit={authSubmitHandler}>
-        {!isLoginMode && (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      <Card className='authentication'>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <h2>{isLoginMode ? 'Log In' : 'Sign Up'}</h2>
+        <hr />
+        <form onSubmit={authSubmitHandler}>
+          {!isLoginMode && (
+            <Input
+              id='name'
+              element='input'
+              type='text'
+              label='Name'
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText='Please enter a name.'
+              onInput={inputHandler}
+            />
+          )}
           <Input
-            id='name'
+            id='email'
             element='input'
-            type='text'
-            label='Name'
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText='Please enter a name.'
+            type='email'
+            label='Email'
+            validators={[VALIDATOR_EMAIL()]}
+            errorText='Please enter a valid email.'
             onInput={inputHandler}
           />
+          <Input
+            id='password'
+            element='input'
+            type='password'
+            label='Password'
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText='Please enter a valid password.'
+            onInput={inputHandler}
+          />
+          <Button type='submit' disabled={!formState.isValid}>
+            {isLoginMode ? 'Log In' : 'Sign Up'}
+          </Button>
+        </form>
+        {isLoginMode ? (
+          <p className='auth__switch-link'>
+            Don't have an account yet?{' '}
+            <span onClick={switchModeHandler}>Sign Up</span>
+          </p>
+        ) : (
+          <p className='auth__switch-link'>
+            Already have an account?{' '}
+            <span onClick={switchModeHandler}>Log In</span>
+          </p>
         )}
-        <Input
-          id='email'
-          element='input'
-          type='email'
-          label='Email'
-          validators={[VALIDATOR_EMAIL()]}
-          errorText='Please enter a valid email.'
-          onInput={inputHandler}
-        />
-        <Input
-          id='password'
-          element='input'
-          type='password'
-          label='Password'
-          validators={[VALIDATOR_REQUIRE()]}
-          errorText='Please enter a valid password.'
-          onInput={inputHandler}
-        />
-        <Button type='submit' disabled={!formState.isValid}>
-          {isLoginMode ? 'Log In' : 'Sign Up'}
-        </Button>
-      </form>
-      {isLoginMode ? (
-        <p className='auth__switch-link'>
-          Don't have an account yet?{' '}
-          <span onClick={switchModeHandler}>Sign Up</span>
-        </p>
-      ) : (
-        <p className='auth__switch-link'>
-          Already have an account?{' '}
-          <span onClick={switchModeHandler}>Log In</span>
-        </p>
-      )}
-    </Card>
+      </Card>
+    </>
   )
 }
 
